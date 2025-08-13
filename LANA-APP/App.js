841fb888062c49e5a364833de7bba4a581c2c3ec
Perusquia Hernandez Jose Miguel <jowsjose6814@gmail.com>
2025-08-12 23:49:27 -0600

    s

--- LANA-APP/App.js
+++ LANA-APP/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
 
+
 export default function App() {
   return (
     <NavigationContainer>
---
+++ LANA-APP/babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [
+    'module:metro-react-native-babel-preset',
+    '@babel/preset-flow'
+  ],
+  plugins: [
+    'react-native-worklets/plugin',
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }]
+  ]
+};
