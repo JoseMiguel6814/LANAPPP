7cbc39d1044be85ef7d40d36671f66d9addd52dc
Perusquia Hernandez Jose Miguel <jowsjose6814@gmail.com>
2025-08-13 00:42:12 -0600

    ssdssd

--- LANA-APP/babel.config.js
+++ LANA-APP/babel.config.js
@@ -7,5 +7,11 @@ module.exports = {
     'react-native-worklets/plugin',
     ['@babel/plugin-transform-private-methods', { loose: true }],
     ['@babel/plugin-transform-class-properties', { loose: true }]
+  ],
+  overrides: [
+    {
+      test: ['./node_modules/victory-native'],
+      presets: ['module:metro-react-native-babel-preset']
+    }
   ]
 };
